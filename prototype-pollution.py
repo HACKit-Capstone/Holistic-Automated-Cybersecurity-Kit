cd5ecf0a27c03e5f9a716a853dcffbb7969bde31
Mohammad-Shoab <88820736+Mohammad-Shoab@users.noreply.github.com>
2023-12-20 12:14:26 +0530

    Update prototype-pollution.py

--- prototype-pollution.py
+++ prototype-pollution.py
@@ -1,8 +1,16 @@
 from os import system
+import sys
 
-msg = input("Enter the target (bing.com): ")
+msg = sys.argv[1]
 
-# system('cat {}.txt | waybackurls > {}urls.txt')
+system(f"cat {msg}urls.txt | httpx -silent -threads 300 | anew -q alive.txt && sed 's/$/\\/?__proto__[testparam]=exploit\\//' alive.txt | page-fetch -j 'window.testparam == \"exploit\" ? \"[VULNERABLE]\" : \"[NOT VULNERABLE]\"' | sed 's/(//g' | sed 's/)//g' | sed 's/JS //g' | grep 'VULNERABLE' > {msg}vuln_proto.txt")
 
-system("cat " + msg + ".txt | httpx -silent -threads 300 | anew -q alive.txt && sed 's/$/\\/?__proto__[testparam]=exploit\\//' alive.txt | page-fetch -j 'window.testparam == \"exploit\" ? \"[VULNERABLE]\" : \"[NOT VULNERABLE]\"' | sed 's/(//g' | sed 's/)//g' | sed 's/JS //g' | grep 'VULNERABLE'")
+system(f"awk '/\[VULNERABLE\]/' {msg}vuln_proto.txt > {msg}proto.txt")
+
 system("rm -rf alive.txt")
+system("rm -rf out")
+system(f"rm -rf {msg}vuln_proto.txt")
+
+print(f"Prototype Pollution vulnerabilities saved to: {msg}proto.txt")
+
+# system(f"cat tee -a {msg}proto.txt | notify")
